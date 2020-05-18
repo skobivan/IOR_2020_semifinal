6cc1b9ec3409a0a2a5908add8ee55037bbe9c66f
skobivan <56694548+skobivan@users.noreply.github.com>
2020-05-19 01:43:50 +0300

    create script

--- IOR_2020/main.py
+++ IOR_2020/main.py
@@ -1,3 +1,79 @@
 import pymurapi as mur
+import time
 
 class PD(object):
+    _kp = 0.0
+    _kd = 0.0
+    _prev_error = 0.0
+    _timestamp = 0
+
+    def __init__(self, kp, kd):
+        self._kp = kp
+        self._kd = kd
+
+    def set_kp(self, value):
+        self._kp = value
+
+    def set_kd(self, value):
+        self._kd = value
+
+    def procces(self, error):
+        timestamp = time.time()
+        output = self._kp * error + self._kd / (timestamp - self._timestamp) * (error - self._prev_error)
+        self._timestamp = timestamp
+        self._prev_error = error
+        return output
+
+def clamp(value, minimum, maximum):
+    if value < minimum:
+        return minimum
+    if value > maximum:
+        return maximum
+    return value
+
+def clamp_motor_speed(value):
+    return clamp(value, -100, 100)
+
+def clamp_angle(angle):
+    if angle > 180:
+        return angle - 360
+    if angle < -180:
+        return angle + 360
+    return angle
+
+class Robot(object):
+
+    def __init__(self, yaw_p, yaw_d, depth_p, depth_d):
+        self.auv = mur.mur_init()
+        self.yaw_pd = PD(yaw_p, yaw_d)
+        self.depth_pd = PD(depth_p, depth_d)
+
+    def keep_yaw(self, yaw, speed):
+            error = self.auv.get_yaw() - yaw
+            error = clamp_angle(error)
+
+            output = self.yaw_pd.procces(error)
+            output = clamp_motor_speed(output)
+
+            self.auv.set_motor_power(0, clamp_motor_speed(speed - output))
+            self.auv.set_motor_power(1, clamp_motor_speed(speed + output))
+
+    def keep_depth(self, depth):
+        error = self.auv.get_depth() - depth
+        output = self.depth_pd.procces(error)
+        output = clamp_motor_speed(output)
+        self.auv.set_motor_power(2, output)
+        self.auv.set_motor_power(3, output)
+
+KP_YAW = 0.8
+KD_YAW = 0.5
+
+KP_DEPTH = 70
+KD_DEPTH = 5
+
+robot = Robot(KP_YAW, KD_YAW, KP_DEPTH, KD_DEPTH)
+time.sleep(2)
+while True:
+    robot.keep_depth(2)
+    robot.keep_yaw(50, 30)
+    time.sleep(0.03)
