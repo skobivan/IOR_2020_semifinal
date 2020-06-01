b8ffa3b330380468e0b073613860b28cb3a4b19d
skobivan <56694548+skobivan@users.noreply.github.com>
2020-06-01 23:05:32 +0300

    front camera

--- IOR_2020/main.py
+++ IOR_2020/main.py
@@ -58,15 +58,41 @@ class Camera:
 class CameraFront(Camera):
     def __init__(self):
         super(CameraFront, self).__init__()
+    
+    def plate_area(self, min_masks, max_masks):
+        copy_curr_img = self.curr_image.copy()
+
+        image_hsv = cv.cvtColor(self.curr_image, cv.COLOR_BGR2HSV)
+
+        image_mask = cv.inRange(image_hsv, min_masks[0], max_masks[0])
+        image_mask += cv.inRange(image_hsv, min_masks[1], max_masks[1])
+        image_mask += cv.inRange(image_hsv, min_masks[2], max_masks[2])
+
+        cnt, _ = cv.findContours(image_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+        total_area = 0
+
+        if cnt:
+            for c in cnt:
+                area = cv.contourArea(c)
+                if abs(area) < 300:
+                    continue
+                total_area += area
+                cv.drawContours(copy_curr_img, [c], 0, (255, 255, 255), 3)
+
+        cv.imshow('mask', image_mask)
+        cv.imshow('mask', copy_curr_img)
+        cv.waitKey(1)
+
+        return total_area
+
 
 class CameraBottom(Camera):
     def __init__(self):
         super(CameraBottom, self).__init__()
 
-    def detect_line(self):
+    def detect_line(self, hsv_mask_min, hsv_mask_max):
         # hsv_mask = {'h_min' : 15, 'h_max' : 30, 's_min' : 50, 's_max' : 255, 'v_min' : 50, 'v_max' : 255}
-        hsv_mask_min = (15, 50, 50)
-        hsv_mask_max = (30, 255, 255)
         hsv_image = cv.cvtColor(self.curr_image, cv.COLOR_BGR2HSV)
 
         mask_image = cv.inRange(hsv_image, hsv_mask_min, hsv_mask_max)
@@ -101,16 +127,17 @@ class CameraBottom(Camera):
         if cnt:
             for c in cnt:
                 area = cv.contourArea(c)
-                if abs(area) < 500:
+                if abs(area) < 1000:
                     continue
                 hull = cv.convexHull(c)
                 approx = cv.approxPolyDP(hull, cv.arcLength(c, True) * 0.02, True)
                 # print(len(approx))
-                if 4 <= len(approx) <= 5:
+                if 4 <= len(approx) <= 5:   
                     ((x, y), (w, h), angle) = cv.minAreaRect(approx)
                     aspect_ratio = w / float(h)
                     # print(aspect_ratio)
                     if not (0.7  <= aspect_ratio <= 1.7):
+                        print(area)
                         cv.drawContours(copy_curr_img, [c], 0, (0, 255, 0), 3)
                         cv.circle(copy_curr_img, (int(x), int(y)), 5, (0, 255, 0))
                         cv.imshow('img', copy_curr_img)
@@ -126,23 +153,41 @@ class CameraBottom(Camera):
         
         return False, 0, 0
 
-        
-
 class Robot(object):
 
     def __init__(self, yaw_p, yaw_d, depth_p, depth_d):
         self.auv = mur.mur_init()
         self.yaw_pd = PD(yaw_p, yaw_d)
         self.depth_pd = PD(depth_p, depth_d)
+
         self.bottom_cam = CameraBottom()
+        self.front_cam = CameraFront()
+        
         self.state = 0
         self.yaw = 0
+        self.depth = 2
         self.speed = 0
+
+        self.hsv_mask_min_orange = (15, 50, 50)
+        self.hsv_mask_max_orange = (30, 255, 255)
+
+        self.hsv_mask_min_yellow = (20, 50, 50)
+        self.hsv_mask_max_yellow = (30, 255, 255)
+        
+        self.hsv_mask_min_green = (40, 40, 20)
+        self.hsv_mask_max_green = (80, 255, 255)
+
+        self.hsv_mask_min_blue = (130, 40, 20)
+        self.hsv_mask_max_blue = (150, 255, 255)
+
+        self.hsv_mask_min_red = (0, 40, 20)
+        self.hsv_mask_max_red = (15, 255, 255)
+
     def is_depth_stable(self, val):
         return abs(self.auv.get_depth()-val) <= 1e-2
 
     def is_yaw_stable(self, val):
-        return abs(self.auv.get_yaw()-val) <= 1e-2
+        return abs(self.auv.get_yaw()-val) <= 1.
 
     def keep_yaw(self, yaw, speed):
             error = self.auv.get_yaw() - yaw
@@ -170,15 +215,19 @@ class Robot(object):
 
     def logic(self):
         self.bottom_cam.update_img(self.auv.get_image_bottom())
-        # self.bottom_cam.detect_rect()
+        self.front_cam.update_img(self.auv.get_image_front())
+
         if self.state == 0:
-            (check, error) = self.bottom_cam.detect_line()
+            self.depth = 2
+            (check, error) = self.bottom_cam.detect_line(self.hsv_mask_min_orange, self.hsv_mask_max_orange)
             if (check):
                 # print(error)
                 self.yaw = -error + self.auv.get_yaw()
-                if self.bottom_cam.detect_line()[0] and abs(self.bottom_cam.detect_line()[1]) < 1e-3:
+                if self.bottom_cam.detect_line(self.hsv_mask_min_orange, self.hsv_mask_max_orange)[0] and \
+                abs(self.bottom_cam.detect_line(self.hsv_mask_min_orange, self.hsv_mask_max_orange)[1]) < 1e-3:
                     self.state += 1
         elif self.state == 1:
+            self.depth = 2
             self.speed = 50
             (check, rect_x, rect_y) = self.bottom_cam.detect_rect()
             # print(self.speed)
@@ -197,12 +246,50 @@ class Robot(object):
                     error -= 90
                     if error < -90:
                         error = error + 180
-                    print(error)
-                    if abs(error) < 1e-2:
+                    # print(error)
+                    if abs(error) < 1.0:
                         self.speed = 50
                     self.yaw = -error + self.auv.get_yaw()
+        elif self.state == 2:
+            self.depth = 2
+            (check, error) = self.bottom_cam.detect_line(self.hsv_mask_min_yellow, self.hsv_mask_max_yellow)
+            if (check):
+                # print(error)
+                self.yaw = -error + self.auv.get_yaw()
+                if self.bottom_cam.detect_line(self.hsv_mask_min_yellow, self.hsv_mask_max_yellow)[0] and \
+                abs(self.bottom_cam.detect_line(self.hsv_mask_min_yellow, self.hsv_mask_max_yellow)[1]) < 1e-3:
+                    self.state += 1
+        elif self.state == 3:
+            self.depth = 3
+            if self.is_depth_stable(3):
+                self.yaw = self.auv.get_yaw() - 90
+                self.state += 1
+        elif self.state == 4:
+            min_area = 1e6
+            min_area_index = 0
+            for i in range(3):
+                while not self.is_yaw_stable(self.yaw):
+                    self.keep_yaw(self.yaw, 0)
+                    self.keep_depth(self.depth)
+                    time.sleep(0.03)
+                self.front_cam.update_img(self.auv.get_image_front())
+                area = self.front_cam.plate_area([self.hsv_mask_min_green, self.hsv_mask_min_red, self.hsv_mask_min_blue], \
+                [self.hsv_mask_max_green, self.hsv_mask_max_red, self.hsv_mask_max_blue])
+                print(area)
+                if area < min_area:
+                    min_area = area
+                    min_area_index = i
+                self.yaw = self.auv.get_yaw() + 90
+            print(min_area, min_area_index)
+            self.yaw = self.auv.get_yaw() - (180-90*min_area_index)
+            self.auv.shoot()
+            self.state += 1
+        elif self.state == 5:
+            self.depth = 0
+            self.speed = 5
+            self.state += 1
         # self.bottom_cam.show()
-        self.keep_depth(2)    
+        self.keep_depth(self.depth)
         self.keep_yaw(self.yaw, self.speed)
 
 KP_YAW = 0.8
@@ -212,7 +299,7 @@ KP_DEPTH = 70
 KD_DEPTH = 5
 
 robot = Robot(KP_YAW, KD_YAW, KP_DEPTH, KD_DEPTH)
-# time.sleep(2)
+
 while not robot.is_depth_stable(2):
     robot.keep_depth(2)
     time.sleep(0.03)
